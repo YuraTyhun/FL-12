54d949a5067b4bdb6222f64fe11a40d2f14013b4
Юрій Тихун <yura_tyhun95@ukr.net>
2020-03-01 22:14:49 +0200

    webinar homework 6

---
+++ FL12_Webinar_HW6/task2/js/index.js
@@ -0,0 +1,74 @@
+const $list = $(".list");
+const $input = $("#add-input");
+const $add = $("#add-submit");
+const $item = $(".item");
+
+let todos = [];
+
+// load data from local storage and display
+if(localStorage.getItem("todoList")) {
+  todos = JSON.parse(localStorage.getItem("todoList"));
+  $(todos).each((i,elem) => {
+    let done = elem.done ? 'done' : '';
+    let listItem = `<li class="item"><span class="item-text ${done}">${elem.text}</span>
+      <button class="item-remove">Remove</button></li>`;
+    $(".list").append(listItem);
+  })
+}
+
+// add new item
+$add.click(function(e) {
+  e.preventDefault();
+  let getInput = $input.val();
+  if(!getInput) {
+    alert('Field is empty. Please, enter new task');
+  } else {
+    let todoItem = {text:getInput, done: false};
+    todos.push(todoItem);
+    let listItem = `<li class="item"><span class="item-text">${getInput}</span>
+      <button class="item-remove">Remove</button></li>`;
+    $(".list").append(listItem);
+    $input.val("");
+    localStorage.setItem("todoList", JSON.stringify(todos));
+  }
+   
+});
+
+// remove item from the list
+$list.on("click", ".item-remove", function(e) {
+  debugger;
+  e.preventDefault();
+  let i = $list.children().index($(this).parent());
+  $(this).parent().remove();
+  todos.splice(i, 1);
+  localStorage.setItem("todoList", JSON.stringify(todos));
+});
+
+// mark tasks as "done" or "undone"
+$(document).on("click", ".item-text", function() {
+  $(this).toggleClass("done");
+  let i = $list.children().index($(this).parent());
+  if(todos[i].done) {
+    todos[i].done = false;
+  } else {
+    todos[i].done = true;
+  }
+  localStorage.setItem("todoList", JSON.stringify(todos));
+});
+
+// search task
+$("#search").click(e => {
+  let value = $("#search-input").val();
+  e.preventDefault();
+  if(!value) {
+    alert('Field is empty. Please, enter smth for search');
+  } else {
+    $(todos).each(() => {
+        $(`.item-text:contains(${value})`).animate({fontSize: "2em"}, 1000,  () => {
+          $(`.item-text:contains(${value})`).animate({fontSize: "1em"}, 2000);
+        });
+    });
+  }
+});
+
+
